4ed60ea2660b2fbeecf41fb5d62cfdfa9c6ac649
Tony Stone <apstone45@gmail.com>
2025-10-16 19:19:51 -0400

    Update and rename SRIVfunctions.py to SRIV.py

--- Ordinary Differential Equations/SRIV.py
+++ Ordinary Differential Equations/SRIV.py
@@ -154,3 +154,18 @@ def SIRV(S0, I0, R0, V0, beta, gamma, nu, iota, l, timesteps, plotChoice1 = None
         analyzeVaccine(F, newNu, newLoss)
 
     return
+
+# We want to compare two proportons
+S, I, R, V = 0, 1, 2, 3
+
+# We want to see the effects of improving the vaccine:
+newVaccinationRates = [.005, .01, .05, .25, .5]
+newVaccineImunityLoss = [.005, 0.025, 0.01, .1, .2]
+
+SIRV(0.95, 0.05, 0, 0, 0.2, .1, .05, .0001, .005, 200, 
+     mainPlot = False,
+     plotChoice1 = S, 
+     plotChoice2 = V, 
+     newNu = newVaccinationRates, 
+     newLoss = newVaccineImunityLoss
+     )
